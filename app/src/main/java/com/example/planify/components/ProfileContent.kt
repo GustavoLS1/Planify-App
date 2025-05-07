2ac229bdedc7f1e1e7a8cbdbde3755ffdbb6bbc6
Jannus Alejandro Largo Zafra <jannus.largo@correounivalle.edu.co>
2025-05-06 22:54:00 -0500

    The view was created to see the user's profile

---
+++ app/src/main/java/com/example/planify/components/ProfileContent.kt
@@ -0,0 +1,67 @@
+package com.example.planify.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.planify.R
+
+@Composable
+fun ProfileContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF1B1A55), shape = RoundedCornerShape(topStart = 48.dp, topEnd = 48.dp))
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 60.dp, bottom = 80.dp)
+        ) {
+            // Foto de perfil superpuesta
+            Box(modifier = Modifier
+                .offset(y = -100.dp)
+                .padding(4.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile), // tu imagen
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                )
+            }
+
+            Text("John Smith", color = Color.White, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(24.dp))
+
+            ProfileOption(icon = Icons.Default.Person, text = "Editar perfil")
+            Spacer(modifier = Modifier.height(15.dp))
+            ProfileOption(icon = Icons.Default.Settings, text = "Configuración")
+            Spacer(modifier = Modifier.height(15.dp))
+            ProfileOption(icon = Icons.Default.ExitToApp, text = "Cerrar sesión")
+        }
+    }
+}
