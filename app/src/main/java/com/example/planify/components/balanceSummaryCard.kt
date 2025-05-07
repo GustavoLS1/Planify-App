d5907bd398c9a568f09b369364e366816bd3bf31
GustavoLS <106133669+GustavoLS1@users.noreply.github.com>
2025-05-07 01:03:16 -0500

    Update balanceSummaryCard

--- app/src/main/java/com/example/planify/components/balanceSummaryCard.kt
+++ app/src/main/java/com/example/planify/components/balanceSummaryCard.kt
@@ -34,9 +34,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.planify.R
+import com.example.planify.ui.theme.FourthColor
+import com.example.planify.ui.theme.PrimaryColor
+import com.example.planify.ui.theme.SecondColor
 import com.example.planify.ui.theme.ThirdColor
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
@@ -59,7 +63,7 @@ fun BalanceSummaryCard(
             contentDescription = "icono de dolar",
             modifier = Modifier
                 .offset(x = 27.dp, y = 7.dp)
-                .size(60.dp)
+                .size(65.dp)
                 .align(Alignment.BottomStart)
             // Puedes cambiar esto según donde lo necesites
         )
@@ -79,68 +83,100 @@ fun BalanceSummaryCard(
                 .padding(start = 35.dp, bottom = 10.dp)
         )
         VerticalDivider(
-            color = Color.Gray,
+            color = FourthColor,
             thickness = 2.dp,
             modifier = Modifier
                 .width(2.dp)
                 .height(140.dp)
                 .offset(x = 123.dp)
         )
-//        Box(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(end = 16.dp)
-//        ) {
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .fillMaxHeight()
+                .padding(end = 16.dp), // Ajusta si necesitas más separación del borde
+            verticalArrangement = Arrangement.Center
+        ) {
+            // INGRESOS
             Column(
                 modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .fillMaxHeight()
-                    .padding(end = 16.dp), // Ajusta si necesitas más separación del borde
-                verticalArrangement = Arrangement.Center
-            ) {
-                // INGRESOS
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalAlignment = Alignment.End
+                    .fillMaxWidth()
+                    .offset(x = 40.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
 
+
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .height(50.dp)
+                        .padding(bottom = 1.dp)
                 ) {
                     Image(
                         painter = painterResource(id = R.drawable.ingresos_tag),
                         contentDescription = "Ingresos",
                         modifier = Modifier
-                            .height(30.dp)
+                            .height(50.dp)
+                            .size(120.dp)
                             .padding(bottom = 1.dp) // Espaciado inferior para separarlo del divider
                     )
+                    if (ingresos.isNotEmpty()) {
+                        Text(
+                            text = ingresos,
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.offset(y = 7.dp)
+                        )
+                    }
                 }
 
-                // DIVIDER HORIZONTAL
-                HorizontalDivider(
-                    color = Color.White,
-                    thickness = 1.dp,
+            }
+
+            // DIVIDER HORIZONTAL
+            HorizontalDivider(
+                color = FourthColor,
+                thickness = 2.dp,
+                modifier = Modifier
+                    .fillMaxWidth(0.6f) // O ajusta según el ancho visual deseado
+                    .padding(vertical = 4.dp)
+                    .align(Alignment.End) // Opcional si quieres alinearlo a la derecha
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(x = 40.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // GASTOS
+                Box(
+                    contentAlignment = Alignment.Center,
                     modifier = Modifier
-                        .fillMaxWidth(0.6f) // O ajusta según el ancho visual deseado
-                        .padding(vertical = 4.dp)
-                        .align(Alignment.End) // Opcional si quieres alinearlo a la derecha
-                )
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .height(50.dp)
+                        .padding(top = 1.dp)
                 ) {
-                    // GASTOS
                     Image(
                         painter = painterResource(id = R.drawable.gastos_tag),
                         contentDescription = "Gastos",
                         modifier = Modifier
-                            .height(30.dp)
+                            .height(50.dp)
+                            .size(120.dp)
                             .padding(top = 1.dp)
                     )
+                    if (gastos.isNotEmpty()) {
+                        Text(
+                            text = gastos,
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.offset(y = 7.dp)
+                        )
+                    }
+
                 }
 
-//            }
-        }
+            }
 
+        }
 
     }
 }
@@ -149,5 +185,5 @@ fun BalanceSummaryCard(
 @Preview
 @Composable
 fun ViewBalanceSummaryCard() {
-    BalanceSummaryCard("$50000", "", "")
+    BalanceSummaryCard("$50000", "10000", "10000")
 }
