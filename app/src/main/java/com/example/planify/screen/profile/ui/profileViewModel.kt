f9de710e1e874e742d8512f253607b5c9111c1a9
MateoZambrano <zambrano.gabriel@correounivalle.edu.co>
2025-05-22 18:40:26 -0500

    Fixed profile screen and add profile view Model

--- app/src/main/java/com/example/planify/screen/profile/ui/profileViewModel.kt
+++ app/src/main/java/com/example/planify/screen/profile/ui/profileViewModel.kt
@@ -1,4 +1,30 @@
 package com.example.planify.screen.profile.ui
 
-class profileViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class profileViewModel: ViewModel() {
+
+    private val _name = mutableStateOf("")
+    val name: State<String> = _name
+
+    private val _number = mutableStateOf("")
+    val number: State<String> = _number
+
+    private val _email = mutableStateOf("")
+    val email: State<String> = _email
+
+    private val _errorMessage = mutableStateOf<String?>(null)
+    val errorMessage: State<String?> = _errorMessage
+
+    fun onProfileChange(
+        name: String,
+        number: String,
+        email: String
+    ){
+        _name.value = name.take(50) // Limita un maximo de 50 caracteres
+        _number.value = number.take(10) // Limita un maximo de 10 caracteres
+        _email.value = email.take(50) // Limita un maximo de 50 caracteres
+    }
 }
